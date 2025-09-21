8c3ac2161aeb3aedb8918c9e728f1e5ed0826ac7
cvschmalz <teenyschool@gmail.com>
2025-09-21 14:05:28 -0300

    Update main.pl
    Adicionei os modos teste e teste local, para ler respostas automaticamente de uma lista de predicados

--- main.pl
+++ main.pl
@@ -81,3 +81,89 @@ exibe_resultado([]).
 exibe_resultado([Pont-Trilha | Resto]) :-
     format("Trilha: ~w - Pontos: ~w~n", [Trilha, Pont]),
     exibe_resultado(Resto).
+
+
+
+% ---------------------------------------------------------
+% Modo teste: carrega respostas de um arquivo, como solicitado no documento
+% (Só funciona se tiver como ler de arquivos separados - não funciona no SWISH)
+teste(Arquivo) :-
+    writeln('Sistema Especialista para Recomendação de Trilha Acadêmica'),
+    writeln('Modo teste'),
+    consulta_arquivo(Arquivo).
+
+% retractall apaga respostas anteriores pra evitar conflitos
+% consult carrega o arquivo especificado e afirma todos os predicados do arquivo
+% Depois, a lógica é igual a em faz_perguntas
+consulta_arquivo(Arquivo) :-
+    retractall(resposta(_, _)),
+    consult(Arquivo),
+    ordena_recomendacoes(Trilhas),
+    writeln("Recomendações:"),
+    exibe_resultado(Trilhas).
+
+% para rodar:
+% ?- teste('perfil_teste_1.pl').
+
+
+
+% -----------------------------------------------------------
+% Modo teste local
+% Carrega dados diretamente pelo mesmo arquivo
+
+% Base de dados:
+
+% Perfil 1: Inteligência Artificial
+perfil_teste(1, [
+    resposta(1,s), resposta(2,s), resposta(3,s), resposta(4,s), resposta(5,s),
+    resposta(6,n), resposta(7,n), resposta(8,n), resposta(9,n), resposta(10,n),
+    resposta(11,n), resposta(12,n), resposta(13,n), resposta(14,n), resposta(15,n),
+    resposta(16,n), resposta(17,n)
+]).
+
+% Perfil 2: Desenvolvimento Web
+perfil_teste(2, [
+    resposta(1,n), resposta(2,n), resposta(3,n), resposta(4,n), resposta(5,n),
+    resposta(6,s), resposta(7,s), resposta(8,s), resposta(9,s), resposta(10,s),
+    resposta(11,n), resposta(12,n), resposta(13,n), resposta(14,n), resposta(15,n),
+    resposta(16,n), resposta(17,n)
+]).
+
+% Perfil 3: Cibersegurança
+perfil_teste(3, [
+    resposta(1,s), resposta(2,n), resposta(3,s), resposta(4,n), resposta(5,n),
+    resposta(6,n), resposta(7,n), resposta(8,n), resposta(9,s), resposta(10,n),
+    resposta(11,s), resposta(12,s), resposta(13,n), resposta(14,n), resposta(15,n),
+    resposta(16,n), resposta(17,n)
+]).
+
+% Perfil 4: Redes e Infraestrutura
+perfil_teste(4, [
+    resposta(1,n), resposta(2,n), resposta(3,n), resposta(4,n), resposta(5,n),
+    resposta(6,n), resposta(7,n), resposta(8,n), resposta(9,s), resposta(10,n),
+    resposta(11,s), resposta(12,s), resposta(13,n), resposta(14,s), resposta(15,s),
+    resposta(16,n), resposta(17,n)
+]).
+
+% Perfil 5: Pesquisador Científico
+perfil_teste(5, [
+    resposta(1,n), resposta(2,n), resposta(3,n), resposta(4,s), resposta(5,n),
+    resposta(6,n), resposta(7,n), resposta(8,n), resposta(9,s), resposta(10,n),
+    resposta(11,n), resposta(12,n), resposta(13,n), resposta(14,s), resposta(15,n),
+    resposta(16,s), resposta(17,s)
+]).
+
+
+% Função principal:
+teste_local(Id) :-
+    writeln('Sistema Especialista - Modo teste'),
+    retractall(resposta(_,_)),
+    perfil_teste(Id, Respostas),
+    forall(member(resposta(Id_R, Resposta), Respostas),
+           assertz(resposta(Id_R, Resposta))),
+    ordena_recomendacoes(Trilhas),
+    writeln("Recomendações:"),
+    exibe_resultado(Trilhas).
+
+% para rodar:
+% ?- teste_local(1).
