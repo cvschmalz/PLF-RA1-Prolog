5816956850f75c3ff06efc9ab295af602162cd5f
Peko <erickms1816@gmail.com>
2025-09-21 14:33:48 -0300

    Create perfil_teste_6.pl

---
+++ perfil_teste_6.pl
@@ -0,0 +1,18 @@
+% Perfil: Pesquisador Científico
+resposta(1, n).
+resposta(2, n).
+resposta(3, n).
+resposta(4, s).  % Inovação
+resposta(5, n).
+resposta(6, n).
+resposta(7, n).
+resposta(8, n).
+resposta(9, s).  % Comunicativa
+resposta(10, n).
+resposta(11, n).
+resposta(12, n).
+resposta(13, n).
+resposta(14, s). % Prática
+resposta(15, n).
+resposta(16, s). % Curiosa
+resposta(17, s). % Questionadora
