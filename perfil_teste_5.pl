beeb1cc4b4e71647a1ff4b43991eee9dcbbcae6e
Peko <erickms1816@gmail.com>
2025-09-21 14:33:19 -0300

    Create perfil_teste_5.pl

---
+++ perfil_teste_5.pl
@@ -0,0 +1,18 @@
+% Perfil: Redes e Infraestrutura
+resposta(1, n).
+resposta(2, n).
+resposta(3, n).
+resposta(4, n).
+resposta(5, n).
+resposta(6, n).
+resposta(7, n).
+resposta(8, n).
+resposta(9, s).  % Comunicativa
+resposta(10, n).
+resposta(11, s). % Redes
+resposta(12, s). % Cibersegurança
+resposta(13, n).
+resposta(14, s). % Prática
+resposta(15, s). % Hardware
+resposta(16, n).
+resposta(17, n).
