85f20c32ffc5bbd181a873f16d275bd778e08f49
Peko <erickms1816@gmail.com>
2025-09-21 14:30:41 -0300

    Create perfil_teste_2.pl

---
+++ perfil_teste_2.pl
@@ -0,0 +1,18 @@
+% Perfil: Desenvolvimento Web
+resposta(1, n).
+resposta(2, n).
+resposta(3, n).
+resposta(4, n).
+resposta(5, n).
+resposta(6, s).  % Visual
+resposta(7, s).  % Criativa
+resposta(8, s).  % Design
+resposta(9, s).  % Comunicativa
+resposta(10, s). % Detalhista
+resposta(11, n).
+resposta(12, n).
+resposta(13, n).
+resposta(14, n).
+resposta(15, n).
+resposta(16, n).
+resposta(17, n).
