70cab0841cd45da46bbc95ea79fee134c8e3821a
Peko <erickms1816@gmail.com>
2025-09-21 14:32:00 -0300

    Create perfil_teste_4.pl

---
+++ perfil_teste_4.pl
@@ -0,0 +1,19 @@
+% Perfil: Ciência de Dados 
+resposta(1, s).  % Organizada
+resposta(2, s).  % Estatística
+resposta(3, s).  % Analítica
+resposta(4, n).
+resposta(5, s).  % Machine Learning
+resposta(6, n).
+resposta(7, n).
+resposta(8, n).
+resposta(9, n).
+resposta(10, n).
+resposta(11, n).
+resposta(12, n).
+resposta(13, s).  % Negócios
+resposta(14, n).
+resposta(15, n).
+resposta(16, n).
+resposta(17, n).
+resposta(17, n).
