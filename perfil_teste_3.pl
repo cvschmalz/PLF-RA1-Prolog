2f8ebb0d7645668f4a022d0a9b6277df00424cd3
Peko <erickms1816@gmail.com>
2025-09-21 14:31:30 -0300

    Create perfil_teste_3.pl

---
+++ perfil_teste_3.pl
@@ -0,0 +1,18 @@
+% Perfil: Cibersegurança
+resposta(1, s).  % Organizada
+resposta(2, n).
+resposta(3, s).  % Analítica
+resposta(4, n).
+resposta(5, n).
+resposta(6, n).
+resposta(7, n).
+resposta(8, n).
+resposta(9, s).  % Comunicativa
+resposta(10, n).
+resposta(11, s). % Redes
+resposta(12, s). % Cibersegurança
+resposta(13, n).
+resposta(14, n).
+resposta(15, n).
+resposta(16, n).
+resposta(17, n).
